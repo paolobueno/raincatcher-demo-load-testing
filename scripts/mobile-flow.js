43b0de487e192d37d09aef466bbc05607814762f
Gerard Ryan <gerard@ryan.lt>
2017-03-01 12:23:12 +0000

    🚧 Start of mobile flow

--- scripts/mobile-flow.js
+++ scripts/mobile-flow.js
@@ -1,9 +1,28 @@
 'use strict';
 
+const _ = require('lodash');
+const Promise = require('bluebird');
+const configureRequest = require('../util/configureRequest');
+const syncDataset = require('../util/syncDataset.js');
+
 module.exports = function mobileFlow(runner, argv) {
   return previousResolution => {
     runner.actStart('Mobile Flow');
-    runner.actEnd('Mobile Flow');
-    return Promise.resolve(previousResolution);
+
+    const baseUrl = argv.app;
+    const clientId = previousResolution.clientIdentifier;
+    const request = configureRequest(clientId, previousResolution.sessionToken);
+    const doSync = syncDataset.bind(this, baseUrl, request, clientId);
+    const datasets = ['workorders', 'workflows', 'messages', 'result'];
+
+    runner.actStart('Device: initialSync');
+    const initialSync = Promise.all(datasets.map(doSync))
+          .then(arr => {
+            runner.actEnd('Device: initialSync');
+            return arr;
+          });
+
+    return initialSync
+      .then(() => runner.actEnd('Mobile Flow'));
   };
 };
