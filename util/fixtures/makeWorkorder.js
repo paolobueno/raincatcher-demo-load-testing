fc278cc9109706f16e0c3a907cc2d32d9fb1c4aa
Gerard Ryan <gerard@ryan.lt>
2017-03-20 16:27:01 +0000

    🚑 Fix setup script to create valid workorders
    JIRA: https://issues.jboss.org/browse/RAINCATCH-635
    
    Up to now, it was using a hardcoded workflow ID, but in the latest
    version of the demo solution, those IDs are different each time.

--- util/fixtures/makeWorkorder.js
+++ util/fixtures/makeWorkorder.js
@@ -6,7 +6,7 @@ module.exports = function makeWorkorder(userId, workflowId) {
     title: `Load test workorder for ${userId}`,
     type: "Job Order",
     status: "New",
-    workflowId: workflowId || "HJ8QkzOSH",
+    workflowId: workflowId,
     address: "The Moon, Earth Orbit",
     location: {
       "0": 4,
@@ -15,6 +15,7 @@ module.exports = function makeWorkorder(userId, workflowId) {
     finishDate: "2029-02-23T00:00:00Z",
     finishTime: "2029-02-23T00:00:00Z",
     summary: "Do the work",
+    // TODO: for some reason 'startTimestamp' now shows up as 'finish date/time' in the portal
     startTimestamp: "2017-02-23T00:00:00Z"
   };
 };
