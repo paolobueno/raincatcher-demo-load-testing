b75204036f164faf5308026e738f0d4aada50d61
Gerard Ryan <gerard@ryan.lt>
2017-03-20 17:09:24 +0000

    🚧 Remove confusing query_params from sync.js
    JIRA: https://issues.jboss.org/browse/RAINCATCH-635
    
    This change also adds additional meta_data properties that are at
    least set for portal requests. and some TODOs too.

--- util/sync.js
+++ util/sync.js
@@ -5,14 +5,15 @@ const requestBodyUtils = require('./sync_request_bodies');
 
 module.exports = function sync(baseUrl, request, clientIdentifier, name) {
   const reqBody = requestBodyUtils.getSyncRequestBody({
+    // TODO: take a dataset_hash as a parameter
+    // TODO: take acknowledgements as a parameter
+    // TODO: just take this entire object as a parameter?
     dataset_id: name,
-    query_params: {
-      "filter": {
-        "key": "assignee",
-        "value": "rkX1fdSH"
-      }
-    },
+    query_params: {}, // TODO: mobile clients filter some datasets here
     meta_data: {
+      sync_frequency: 5,
+      storage_strategy: "dom",
+      do_console_log: false,
       clientIdentifier: clientIdentifier
     },
     pending: []
