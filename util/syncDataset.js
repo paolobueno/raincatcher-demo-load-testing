bce188e820eb6f678fb7d241b74fffd646b6ea17
Gerard Ryan <gerard@ryan.lt>
2017-03-23 12:12:07 +0000

    🚧 Add a function to update clientRecs
    JIRA: https://issues.jboss.org/browse/RAINCATCH-635

--- util/syncDataset.js
+++ util/syncDataset.js
@@ -2,10 +2,24 @@
 
 const _ = require('lodash');
 
+
+function updateClientRecs(inRecs, syncRecordsResponse) {
+  const deletedRecs = _.keys(syncRecordsResponse.delete);
+  const createdRecs = _.transform(syncRecordsResponse.create, (acc, rec) => acc[rec.data.id] = rec.hash);
+  const updatedRecs = _.transform(syncRecordsResponse.update, (acc, rec) => acc[rec.data.id] = rec.hash);
+  const outRecs = _.merge(_.omit(inRecs, deletedRecs), createdRecs, updatedRecs);
+
+  return outRecs;
+}
+
 module.exports = function syncDataset(baseUrl, request, clientId, dataset, previousSyncRecordsResult) {
 
+  // TODO: use `updateClientRecs` function above so that updates and deletes are handled
+  // it'll mean that 'clientRecs' will need to be passed into (and out of) this function
+  // Maybe that updateClientRecs function should be somewhere else than this function, and just pass in the pre-processed clientRecs here, and not previousSyncrecordsresult (and old clientRecs)
   const creations = _.get(previousSyncRecordsResult, 'create', {});
   const clientRecs = _.transform(creations, (out, v, k) => out[k] = v.hash);
+
   const payload = {
     fn: 'syncRecords',
     dataset_id: dataset,
