022831e970b4a7a1d1729dbd45c3013c8717f2e9
Gerard Ryan <gerard@ryan.lt>
2017-03-23 12:11:02 +0000

    ✨ Add sync loop with interval to createRecord
    JIRA: https://issues.jboss.org/browse/RAINCATCH-635
    
    This is just to make sure that we wait until we hear from the server
    that it has applied our update

--- util/createRecord.js
+++ util/createRecord.js
@@ -3,11 +3,42 @@
 const recordUtils = require('./generate_record');
 const makeSyncBody = require('./fixtures/makeSyncBody');
 const sync = require('./sync');
+const Promise = require('bluebird');
+const _ = require('lodash');
+
+function isApplied(hash, syncResult) {
+  return _.find(_.get(syncResult, 'updates.applied', {}), {'hash': hash}) || false;
+}
+
+function syncLoop(mainFn, compareFn, interval, initialSyncResult) {
+  return new Promise(resolve => {
+
+    function next(previousResult) {
+      mainFn(previousResult).then(result => {
+        if (compareFn(result)) {
+          return resolve(result);
+        } else {
+          return Promise.delay(interval).then(() => next(result));
+        }
+      });
+    }
+
+    return next(initialSyncResult);
+  })
+  // Loop for a max of 2 minutes
+    .timeout(120000);
+}
 
 module.exports = function createRecord(baseUrl, request, clientId, dataset, data, dataset_hash, query_params, acknowledgements) {
 
   const pending = [recordUtils.generateRecord(data)];
   const payload = makeSyncBody(dataset, clientId, dataset_hash, query_params, pending, acknowledgements);
 
-  return sync(request, `${baseUrl}/mbaas/sync/${dataset}`, payload);
+  // This just partially applies sync so that it can be passed to the sync loop in the `.then` below
+  const syncp = sync.bind(null, request, `${baseUrl}/mbaas/sync/${dataset}`, makeSyncBody(dataset, clientId, dataset_hash, query_params, null, acknowledgements));
+
+  return sync(request, `${baseUrl}/mbaas/sync/${dataset}`, payload)
+  // then loop until server says it applied the changes
+  // TODO: should *all* datasets be synced as part of this loop, or is just the relevant one enough?
+    .then(syncDatasetResult => syncLoop(syncp, isApplied.bind(null, payload.pending[0].hash), 5000, syncDatasetResult));
 };
