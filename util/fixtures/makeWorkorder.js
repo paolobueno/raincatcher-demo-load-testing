6f181a76a557d6850899a0a8b99c4a2c9bdc443e
Gerard Ryan <gerard@ryan.lt>
2017-03-29 10:50:56 +0100

    🐛 Set startTimestamp to future since it's finish
    JIRA: https://issues.jboss.org/browse/RAINCATCH-635

--- util/fixtures/makeWorkorder.js
+++ util/fixtures/makeWorkorder.js
@@ -15,7 +15,6 @@ module.exports = function makeWorkorder(userId, workflowId) {
     finishDate: "2029-02-23T00:00:00Z",
     finishTime: "2029-02-23T00:00:00Z",
     summary: "Do the work",
-    // TODO: for some reason 'startTimestamp' now shows up as 'finish date/time' in the portal
-    startTimestamp: "2017-02-23T00:00:00Z"
+    startTimestamp: "2018-02-23T00:00:00Z"
   };
 };
